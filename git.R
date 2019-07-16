7a8c0981b15599947d2e5b036368e360f1226d94
Grzegorz Piwowarczyk <grzegorz.piwowarczyk@pl.ibm.com>
2019-07-16 16:45:31 +0200

    porządki i funkcja range

--- git.R
+++ git.R
@@ -1,8 +1,17 @@
 w1 <- c(1,2,3,4,5,6,6,7,8,9,10)
 w2 <- c(4,5,5,5,6,6,6,6,7,7,8,8)
+#Współpraca z Githubem
 install.packages("devtools")
 library(devtools)
 install_github('karabanb/BKmisc')
 1
 library(BKmisc)
 #standard deviations
+
+range_gp <- function(variables) {
+  max(viariables) - min(variables)
+}
+# unikamy pokrywania się nazw funkcji
+#jeżeli nazwy funkcji się pokrywają
+#wskazujemy z którego pakietu ma brać funkcję
+#poprzez "nazwa_pakietu::funkcja"
