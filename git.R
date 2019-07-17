c381a17705fc8fca957560d65c7b7cfe39528395
Grzegorz Piwowarczyk <grzegorz.piwowarczyk@pl.ibm.com>
2019-07-17 10:19:16 +0200

    cleaning notes

--- git.R
+++ git.R
@@ -2,31 +2,35 @@
 
 install.packages("devtools")
 library(devtools)
-install_github('karabanb/BKmisc')
+install_github('karabanb/BKmisc') #laduje paczke Bartka
 1
 library(BKmisc)
 
-#standard deviations
+#Descriptive Statistics -----------------
 
+# range --  obserwacja max - obserwacja min
 range_gp <- function(variables) {
   max(viariables) - min(variables)
 }
-# unikamy pokrywania się nazw funkcji
+#unikamy pokrywania się nazw funkcji
 #jeżeli nazwy funkcji się pokrywają
 #wskazujemy z którego pakietu ma brać funkcję
 #poprzez "nazwa_pakietu::funkcja"
 
+
 w1 <- c(1,2,3,4,5,6,6,7,8,9,10)
 w2 <- c(4,5,5,5,6,6,6,6,7,7,8,8)
 
-quantile(w1)
+quantile(w1) #quantiles
 
-stand_dev1 <- sd(w1)
-srednia1 <- mean(w1)
 
-stand_dev2 <- sd(w2)
-srednia2 <- mean(w2)
+sd1 <- sd(w1) #standard deviation
+mean1 <- mean(w1) #mean
+sd1/mean1*100 #wspolczynik zmienności?? -- standard deviation / mean
 
-stand_dev1/srednia1*100
-stand_dev2/srednia2*100
+sd2 <- sd(w2)
+mean2 <- mean(w2)
+sd2/mean2*100
+
+
 
