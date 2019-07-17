137f804ea265b9b1c055e66c1c5ead8bdecc60e7
GrzegorzPiwowarczyk <52694530+GrzegorzPiwowarczyk@users.noreply.github.com>
2019-07-17 14:15:08 +0200

    Delete DescriptiveStatistics

--- DescriptiveStatistics.R
+++
@@ -1,8 +0,0 @@
-w1 <- c(1,2,3,4,5,6,6,7,8,9,10)
-w2 <- c(4,5,5,5,6,6,6,6,7,7,8,8)
-install.packages("devtools")
-library(devtools)
-install_github('karabanb/BKmisc')
-1
-library(BKmisc)
-#standard deviations
